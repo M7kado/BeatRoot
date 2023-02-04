40e91dedae8e3f00b2af64b5e9b6a947f5eafabc
Basile <bourgue.basile@gmail.com>
2023-02-04 13:53:21 +0100

    pas grand chose

--- Assets/Scripts/Beetroot.cs
+++ Assets/Scripts/Beetroot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : Clockable
+public class Beetroot : Clockable
 {
     public enum State { Empty, Growing, Grown, Rotten}
     State state = new();
